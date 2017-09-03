e9a265831b6210cd65ca9fd4e92ade0300a8f6d1
Steven Winston <steven@elevationacademy.co>
2017-09-03 10:16:02 +0300

    added tests

---
+++ test.js
@@ -0,0 +1,13 @@
+var elvAc_roster = {
+    address: “WeWork Hazerem”,
+    cohort: 6,
+    names:{
+        girls:[“lana”,“lise”,“anna”,“andrea”,“sarai”],
+        boys:[“shy”,“yitzchak”,“amitay”,“dan”,“ivgeny”,“daniel”,“olin”,“ahron”,“avi”]
+    }
+}
+
+elvAc_roster.numOfStudents = function(){
+    return elvAc_roster.names.girls.length + elvAc_roster.names.boys.length;
+}
+elvAc_roster.numOfStudents();
